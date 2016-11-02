ee074cb0e22bda5dc5dacc6d3ac49c5e981d08fe
legusx <legusscratcher@gmail.com>
2016-11-02 00:58:09 +0000

    Added new commands

--- bot.py
+++ bot.py
@@ -41,22 +41,35 @@ def on_message(message):
 
 
   if message.content.upper() == '$ISADMIN':
-    if UID in badlist:
-      pass
-    elif UID in adlist:
-      yield from client.send_message(c, 'Yes, you have full permissions.')
+    if UID in adlist:
+      yield from client.send_message(c, 'Yes, you are a bot admin.')
     else:
-      yield from client.send_message(c, 'No, you do not have access to all bot commands. If you think this is a mistake please contact <@190313064367652864>')
+      if UID in badlist:
+        pass
+      else:
+        yield from client.send_message(c, 'No, you do not have access to all bot commands. If you think this is a mistake please contact <@190313064367652864>')
 
   if message.content.startswith('$BLIST'):
     if UID in adlist:
       mes = mes.split(' ')
       if len(mes) > 0:
-        yield from client.send_message(authr, badlist)
+        yield from client.send_message(auth, badlist)
       else:
         if mes[1].upper() == 'ADD':
           badlist.append(mes[2])
         elif mes[1].upper() == 'REM':
           badlist.remove(mes[2])
+
+  if message.content.upper() == '$INFO':
+    if UID in badlist:
+      pass
+    else:
+      yield from client.send_message(c, 'HordesBot was created by BlazingFire007 and LegusX.')
+
+  if message.content.upper() == '$HELP':
+    if UID in badlist:
+      pass
+    else:
+      yield from client.send_message(c, '`HORDESBOT HELP: \nEveryone:\n$INFO - Gives info about HordesBot. \n$ISADMIN - Tells you if you have admin privelages.\n$PING - Returns with "PONG". Used to ensure HordesBot is running.')
 #Replace TOKEN with the actual token.
 client.run("MjQzMTIwMTM3MDEwNDEzNTY4.CvqzgQ.F_LYyrZj3h10eW20poqWdsxL1Vc")
