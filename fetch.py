bc23c8dfbc304223e305afc76744620a630e692a
BlazingFire007 <elijahrich02@icloud.com>
2016-11-13 13:59:31 -0600

    Create fetch.py
    Basic template for data fetching. May switch to use requests library in the future.

---
+++ fetch.py
@@ -0,0 +1,22 @@
+import socket, sys, os, time
+#Set host and port
+host = 'www.hordes.io'
+port = 80
+
+#Define socket and connect.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+
+#Send Message
+message = 'PLACEHOlDER'
+try:
+    s.send(message.encode())
+except as err:
+    print('An error has occured\n' + err)
+
+#Receive data
+try:
+    data = s.recv(2048).decode()
+except as err:
+    print('An error has occured\n' + err)
+s.close()
